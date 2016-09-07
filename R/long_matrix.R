b409c7326478438461552d0289e7da158a9633e8
Greg Macfarlane <gregmacfarlane@gmail.com>
2016-09-07 10:22:23 -0400

    Add an option to set value name

--- R/long_matrix.R
+++ R/long_matrix.R
@@ -1,6 +1,8 @@
 #' Transform a matrix into long format
 #'
 #' @param matrix An R matrix with row and column names or indices
+#' @param value A character string identifying the name of the value column in
+#'   the output data_frame
 #'
 #' @return A \code{data_frame} with row and column ids and matrix values.
 #'
@@ -8,22 +10,24 @@
 #'
 #' @export
 #'
-long_matrix <- function(matrix){
+long_matrix <- function(matrix, value = NULL){
 
   # set matrix row and column names if the exist
   names <- dimnames(matrix)
   if(is.null(names[1])){ rnames <- 1:nrow(matrix) } else { rnames <- names[[1]] }
   if(is.null(names[2])){ cnames <- 1:ncol(matrix) } else { cnames <- names[[2]] }
 
+  # if no value name given, default to "value"
+  if(is.null(value)) value <- "value"
 
   # turn matrix into a data_frame with row names as an origin ID and
   # column names as variables
   df <- matrix %>% as.data.frame()
   names(df) <- cnames
-  df <- dplyr::bind_cols(data_frame(origin = rnames), df)
+  dplyr::bind_cols(data_frame(origin = rnames), df) %>%
 
-  # longify
-  df %>%
-    tidyr::gather(destination, value, -origin, convert = TRUE)
+    # longify
+    tidyr::gather_("destination", value, cnames, convert = TRUE)
+
 
 }
