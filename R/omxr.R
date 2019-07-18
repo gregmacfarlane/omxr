5f288b9951fac9730c1c15ca82444d790e837c5e
Greg Macfarlane <gregmacfarlane@gmail.com>
2019-07-18 10:04:07 -0600

    Add function to get package examples
    This is how they do things in the readr package.

--- R/omxr.R
+++ R/omxr.R
@@ -15,3 +15,22 @@ NULL
 
 ## quiets concerns of R CMD check re: the .'s that appear in pipelines
 if(getRversion() >= "2.15.1")  utils::globalVariables(c(".", "value"))
+
+
+#' Get path to example file
+#' 
+#' omxr comes bundled with an example omx and zmx file in its `inst/extdata`
+#' directory. This function makes it easy to access.
+#' 
+#' @param path Name of file. If `NULL`, the example files will be listed.
+#' @export
+#' @examples
+#' omxr_example()
+#' omxr_example("test.omx")
+omxr_example <- function(path = NULL) {
+  if (is.null(path)) {
+    dir(system.file("extdata", package = "omxr"))
+  } else {
+    system.file("extdata", path, package = "omxr", mustWork = TRUE)
+  }
+}
